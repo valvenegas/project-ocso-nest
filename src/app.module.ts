9db0dbe25da599e103256d13a6c8f930492faa78
Val Venegas <venegasruizdulcevaleria0@gmail.com>
2024-09-21 18:08:53 -0600

    AllMethodsImplement

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.host,
+    port: +process.env.port,
+    username: 'postgres',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities:[],
+    autoLoadEntities: true,
+    synchronize: true,
+  }), EmployeesModule, ProductsModule],
+  
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,7 @@
-import { IsInt, IsNumber, IsOptional, IsString, IsUUID, MaxLength } from "class-validator";
+import { IsInt, IsNumber, IsOptional, isString, IsString, IsUUID, MaxLength } from "class-validator";
 
 export class CreateProductDto {
+    @IsString()
     @IsUUID("4")
     @IsOptional()
     productId: string;
