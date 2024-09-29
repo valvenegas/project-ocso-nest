d6a1a51bd1ac1028831dba912a4c93b81f1714e5
Val Venegas <venegasruizdulcevaleria0@gmail.com>
2024-09-28 22:44:07 -0600

    NewConfigs

--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,14 @@
-export class Location {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn('increment')
+    locationId: number;
+    @Column('text')
+    locationName: string;
+    @Column('text')
+    locationAdress: string;
+    @Column('array')
+    locationLating: number[];
+
+}
