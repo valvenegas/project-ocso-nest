e1b6d9eefaa43016a8eae497330e3378c7c53678
Val Venegas <venegasruizdulcevaleria0@gmail.com>
2024-10-05 22:51:28 -0600

    decorator

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,14 +1,17 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class User{
+export class User {
     @PrimaryGeneratedColumn('uuid')
     userId: string;
     @Column('text')
-    userEmail:string;
+    userEmail: string;
     @Column('text')
     userPassword: string;
-
+    @Column('text', {
+        default: ["Employee"]
+    })
+    userRoles: string[];
 
 
 }
--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UseGuards, UnauthorizedException } from '@nestjs/common';
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { UserData } from 'src/auth/decorators/user.decorators';
+import { User } from 'src/auth/entities/user.entity';
 
 @Controller('providers')
 export class ProvidersController {
@@ -15,7 +17,8 @@ export class ProvidersController {
 
   @UseGuards(AuthGuard)
   @Get()
-  findAll() {
+  findAll(@UserData() user: User) {
+    if(!user.userRoles) throw new UnauthorizedException("No estas autorizado")
     return this.providersService.findAll();
   }
   @Get('/name/:name')
