2a3fbecc5310efb172316f7cf1f0a1bb7224bd2d
Val Venegas <venegasruizdulcevaleria0@gmail.com>
2024-10-19 21:00:26 -0600

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors:{
+      origin: process.env.allowedOrigin
+    }
+  });
   const config = new DocumentBuilder()
   .setTitle('Ocso API')
   .setDescription('API for ocso managment')
@@ -18,6 +22,6 @@ SwaggerModule.setup('api', app, document);
     forbidNonWhitelisted: true,
 
 }))
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
